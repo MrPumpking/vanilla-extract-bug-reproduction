46992e9a59aa841a3e46361646a7aec62d743e04
Michał Bar <logout400@gmail.com>
2022-01-27 18:35:10 +0100

    feat: add working example

--- libs/feature/domain1/feature1/component1/src/item/ExampleListItem.tsx
+++ libs/feature/domain1/feature1/component1/src/item/ExampleListItem.tsx
@@ -1,4 +1,4 @@
-import * as styles from './ExampleListItem.css';
+import * as styles from '../ExampleListItem.css';
 
 export const ExampleListItem: React.FC = () => {
   return <li className={styles.root}>Example Item</li>;
